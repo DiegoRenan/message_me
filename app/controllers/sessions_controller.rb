1dda9898e5b96f03382db19945b8d9527bf9c6b4
Renan Diego <renan.ccomp@gmail.com>
2023-05-20 14:05:24 -0300

    login user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = 'Logged in successfully'
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'There was something wrong'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'User logout'
+    redirect_to root_path
+  end
 end
