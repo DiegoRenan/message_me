f2f56f21d6db933979444a9f4d62c6b9bf469baa
Renan Diego <renan.ccomp@gmail.com>
2023-05-21 14:02:37 -0300

    chatroom messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+
+  def require_user
+    if !logged_in?
+      flash[:error] = "You must loggin"
+      redirect_to login_path
+    end
+  end
 end
--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -2,14 +2,14 @@ import consumer from "channels/consumer"
 
 consumer.subscriptions.create("ChatroomChannel", {
   connected() {
-    // Called when the subscription is ready for use on the server
+    console.log("Connection ready!")
   },
 
   disconnected() {
-    // Called when the subscription has been terminated by the server
+    console.log("Disconnected ready!")
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    alert(data.foo)
   }
 });
